97a28709a34d40ec437efb8840356efe1e3eee20
dTum <50366412+dtumanenkov@users.noreply.github.com>
2020-05-23 17:32:38 +0300

    task 3.1 шаблонизатор

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 $is_auth = rand(0, 1);
 $text_max_symbols_number=300;//максимальная длина текстового содержимого карточки поста
 $user_name = 'Denis'; // укажите здесь ваше имя
+$page_title='Readme';
 $posts=[
     [
         'post_header'=>'Цитата',
@@ -60,8 +61,25 @@ function trim_text($str,$symbols_number=300){ //функция для обрез
     }
     return $trim_str;
 }
+function include_template($name_of_template, array $data = [])
+{
+    $name_of_template = 'templates/' . $name_of_template;
+    $result = '';
 
-include_template($posts);
-include_template(\templ)
+    if (!is_readable($name_of_template)) {
+        return $result;
+    }
+
+    ob_start();
+    extract($data);
+    require $name_of_template;
+
+    $result = ob_get_clean();
+
+    return $result;
+}
+$page_content=include_template('main.php',['posts'=>$posts,'text_max_symbols_number'=>$text_max_symbols_number]);
+$layout_content=include_template('layout.php',['user_name'=>$user_name,'page_title'=>$page_title,'is_auth'=>$is_auth,'page_content'=>$page_content]);
+print($layout_content);
 ?>
 
