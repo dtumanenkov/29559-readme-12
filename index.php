be4c5043aacd517086bfcba4a94d127e6fbc7924
dTum <50366412+dtumanenkov@users.noreply.github.com>
2020-05-26 23:36:46 +0300

    3.2 замечания + PSR2 первая попытка

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
+require_once('init.php');
+require_once('helpers.php');
 $is_auth = rand(0, 1);
-$text_max_symbols_number=300;//максимальная длина текстового содержимого карточки поста
+$text_max_symbols_number = 300;//максимальная длина текстового содержимого карточки поста
 $user_name = 'Denis'; // укажите здесь ваше имя
-$page_title='Readme';
+$page_title = 'Readme';
 $posts=[
     [
         'post_header'=>'Цитата',
@@ -42,70 +44,72 @@ $posts=[
         'avatar'=>'userpic.jpg'
     ],
 ];
-function trim_text($str,$symbols_number=300){ //функция для обрезки длинного текста
-    $trim_str="";//переменная для работы со строкой
-    if(strlen($str)<$symbols_number){   //если длина меньше 300 -  просто возвращаем строку
-        $trim_str=$str;
-    }
-    else{
-        $str_array=explode(" ",$str); // разбиваем исходную строку на массив с разделителем пробелом
-        $total_length=0;// счетчик длины строки
-        for ($i=0;$i<count($str_array);$i++){ //пройдем по массиву подсчитывая длину каждого элемента
-            $total_length=$total_length+strlen($str_array[$i])+1;//общая длина слов массива + 1 символ пробела в конце каждого слова
-            if($total_length>$symbols_number){//если выбиваемся за предел количества символов - значит текущее слово уже не влезет
-                $trim_str=array_slice($str_array,0,$i); //обрезаем массив по i-тому элементу(не i-1 - т.к отсчет массива начинается с 0!)
+function trim_text($str,$symbols_number = 300){ //функция для обрезки длинного текста
+    $trim_str = "";//переменная для работы со строкой
+    if (strlen($str) < $symbols_number) {   //если длина меньше 300 -  просто возвращаем строку
+        $trim_str = $str;
+    } else {
+        $str_array = explode(" ",$str); // разбиваем исходную строку на массив с разделителем пробелом
+        $total_length = 0;// счетчик длины строки
+
+        for ($i = 0; $i < count($str_array); $i++) { //пройдем по массиву подсчитывая длину каждого элемента
+            $total_length = $total_length + strlen($str_array[$i]) + 1;//общая длина слов массива + 1 символ пробела в конце каждого слова
+
+            if($total_length > $symbols_number) {//если выбиваемся за предел количества символов - значит текущее слово уже не влезет
+                $trim_str = array_slice($str_array,0,$i); //обрезаем массив по i-тому элементу(не i-1 - т.к отсчет массива начинается с 0!)
                 break; // прерываем цикл for
             }
         }
-        $trim_str=implode(" ",$trim_str).'...'; //собираем обрезанный массив обратно в строку
+        $trim_str = implode(" ",$trim_str).'...'; //собираем обрезанный массив обратно в строку
     }
     return $trim_str;
 }
 function time_delta($post_time){    //подсчет разницы времени между публикацией и текущей датой
-    $ts=time();
-    $sec_in_hour=3600;
-    $sec_in_day=86400;
-    $sec_in_week=604800;
-    $sec_in_month=2419200;
-    $end_ts=strtotime($post_time);
-    $ts_diff=$ts-$end_ts;
-    if($ts_diff<=60){ //менее 60 сек
+    $ts = time();
+    $end_ts = strtotime($post_time);
+    $ts_diff = $ts - $end_ts;
+
+    if ($ts_diff <= 60) { //менее 60 сек
         return 'менее минуты назад';
-    }
-    elseif($ts_diff<=3600 && $ts_diff>60){ //менее 60 минут
-        return floor($ts_diff/60).get_noun_plural_form(floor($ts_diff/60),' минуту назад',' минуты назад',' минут назад');
-    }
-    elseif($ts_diff<=86400 && $ts_diff>3600){ //менее 24 часов но более 60 минут
-        return floor($ts_diff/$sec_in_hour).get_noun_plural_form(floor($ts_diff/$sec_in_hour),' час назад',' часа назад',' часов назад');
-    }
-    elseif($ts_diff<=604800 && $ts_diff>86400) {//менее 7 дней, но больше 24 часов
-        return floor($ts_diff/$sec_in_day).get_noun_plural_form(floor($ts_diff/$sec_in_day),' день назад',' дня назад',' дней назад');
-    }
-    elseif($ts_diff<=2419200 && $ts_diff>604800) {//менее 5 недель, но больше 7 дней
-        return floor($ts_diff/$sec_in_week).get_noun_plural_form(floor($ts_diff/$sec_in_week),' неделю назад',' недели назад',' недель назад');
-    }
-    else {
-        return floor($ts_diff/$sec_in_month).get_noun_plural_form(floor($ts_diff/$sec_in_month),' месяц назад',' месяца назад',' месяцев назад');
+
+    } elseif ($ts_diff <= 3600 && $ts_diff > 60) { //менее 60 минут
+
+        return floor($ts_diff / 60).get_noun_plural_form(floor($ts_diff / 60),
+                ' минуту назад',' минуты назад',' минут назад');
+    } elseif ($ts_diff <= 86400 && $ts_diff > 3600) { //менее 24 часов но более 60 минут
+
+        return floor($ts_diff / sec_in_hour).get_noun_plural_form(floor($ts_diff / sec_in_hour),
+                ' час назад',' часа назад',' часов назад');
+    } elseif ($ts_diff <= 604800 && $ts_diff > 86400) {//менее 7 дней, но больше 24 часов
+
+        return floor($ts_diff / sec_in_day).get_noun_plural_form(floor($ts_diff / sec_in_day),
+                ' день назад',' дня назад',' дней назад');
+    } elseif ($ts_diff <= 2419200 && $ts_diff > 604800) {//менее 5 недель, но больше 7 дней
+
+        return floor($ts_diff / sec_in_week).get_noun_plural_form(floor($ts_diff / sec_in_week),
+                ' неделю назад',' недели назад',' недель назад');
+    } else {
+        return floor($ts_diff / sec_in_month).get_noun_plural_form(floor($ts_diff / sec_in_month),
+                ' месяц назад',' месяца назад',' месяцев назад');
     }
     //доделать второй вариант потом.
- /*   $date_now=date_create("now");
-    $post_time=date_create($post_time);
-    $diff=date_diff($date_now,$post_time);
-    if(($diff->days)>35){ //берем свойство days из объекта типа DateInterval
-        $delta=date_interval_format($diff,"%m").' месяцев';
-    }
-    elseif (($diff->days)<35 && ($diff->days)>7){
+ /*   $date_now = date_create("now");
+    $post_time = date_create($post_time);
+    $diff = date_diff($date_now,$post_time);
+    if(($diff -> days) > 35){ //берем свойство days из объекта типа DateInterval
+        $delta = date_interval_format($diff,"%m").' месяцев';
+    } elseif (($diff -> days) < 35 && ($diff -> days) > 7){
         $delta=date_interval_format($diff,"%m").' недель';
-    }
-    else {
+    } else {
         $delta=date_interval_format($diff,"%m").' дней';
     }
     return $delta;
  */
 }
-require_once('helpers.php');
-$page_content=include_template('main.php',['posts'=>$posts,'text_max_symbols_number'=>$text_max_symbols_number]);
-$layout_content=include_template('layout.php',['user_name'=>$user_name,'page_title'=>$page_title,'is_auth'=>$is_auth,'page_content'=>$page_content]);
+$page_content = include_template('main.php',['posts' => $posts,
+    'text_max_symbols_number' => $text_max_symbols_number]);
+$layout_content = include_template('layout.php',['user_name' => $user_name,'page_title' => $page_title,
+    'is_auth' => $is_auth,'page_content' => $page_content]);
 print($layout_content);
 ?>
 
---
+++ init.php
@@ -0,0 +1,7 @@
+<?php
+date_default_timezone_set("Europe/Moscow"); // временная зон апо умолчанию
+define("sec_in_hour",3600); // константы времени
+define("sec_in_day",86400);
+define("sec_in_week",604800);
+define("sec_in_month",2419200);
+
